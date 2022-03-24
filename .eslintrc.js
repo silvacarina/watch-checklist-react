b5b812b2c99aaabf2d9b0e25ad554070363f63c3
Carina <cass.carinasilva@gmail.com>
2022-03-24 11:42:55 -0300

    Alterado configurações do eslint, instalado pacotes e alterado favico

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,26 +1,28 @@
 module.exports = {
-    env: {
-      browser: true,
-      es2021: true,
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
     },
-    extends: [
-      'plugin:react/recommended',
-      'airbnb',
-    ],
-    parserOptions: {
-      ecmaFeatures: {
-        jsx: true,
-      },
-      ecmaVersion: 'latest',
-      sourceType: 'module',
-    },
-    plugins: [
-      'react',
-    ],
-    rules: {
-      'linebreak-style': 0,
-      'jsx-a11y/label-has-associated-control': 0,
-  
-    },
-  };
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+  ],
+  rules: {
+    'linebreak-style': 0,
+    'jsx-a11y/label-has-associated-control': 0,
+    'prefer-arrow-callback': 0,
+    'no-else-return': 0,
+    'func-names': 0
+  },
+};
 
